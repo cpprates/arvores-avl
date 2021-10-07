2d0c762e319bc7c2374cdeeb03085a02ed73b4d6
Carolina <cpprates18@gmail.com>
2021-10-07 00:43:49 -0300

    adding menu

--- src/main/java/com/arvores_avl/Main.java
+++ src/main/java/com/arvores_avl/Main.java
@@ -1,5 +1,7 @@
 package com.arvores_avl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
@@ -10,5 +12,45 @@ public class Main {
         tree.addNode(new Tree(50));
         tree.addNode(new Tree(60));
         tree.addNode(new Tree(70));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the AVL Tree Application\n Choose your option to start:");
+
+        int option;
+        do {
+
+            printOptions();
+            option = scanner.nextInt();
+
+            printReceiveParam();
+            int key = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println(
+                            tree.addNode(new Tree(key)) ? "Error. Choose another key" : "Print tree after modifying");
+                    // print da árvore
+                    break;
+                case 2:
+                    System.out.println(tree.contains(new Node(key)) ? "Key Found" : "Key Not found");
+                    // apresentar a lista de nós consultados
+                    break;
+                case 3:
+                    // comentado pq ainda n tem o método
+                    // System.out.println(tree.remove(new Node(key)) ? "Removed successfully" :
+                    // "Error");
+                    // print da árvore
+                    break;
+            }
+        } while (option != 4);
+        System.out.println("Exiting Application");
+    }
+
+    public static void printOptions() {
+        System.out.println("1. Insert | 2. Search | 3. Remove | 4. Exit");
+    }
+
+    public static void printReceiveParam() {
+        System.out.println("Type the Integer key value:");
     }
 }
